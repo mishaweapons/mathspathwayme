444bd5769b6b52e154d9417d684167fd2e5be9ff
mishaweapons <SELL0007@footscray.vic.edu.au>
2025-02-14 15:13:47 +1100

    Create mathspathwayme.js

---
+++ mathspathwayme.js
@@ -0,0 +1,13 @@
+// *Copyright mishaweapons 2025
+//
+//
+if (window.confirm("Would you like to proceed with modifying the tab?"))
+{ document.title = "Maths Pathway";
+var link = document.querySelector("link[rel~='icon']");
+if (!link) { link = document.createElement('link'); link.rel = 'icon';
+document.head.appendChild(link); }
+link.href = "https://mathspathway.com/wp-content/uploads/2022/06/math-pathway-favicon-150x150.png"; }
+alert("Sent a request to change the icon. Please note some sites will not let you do this due to their security policy. However, the title will change no matter what.")
+//
+//
+//
